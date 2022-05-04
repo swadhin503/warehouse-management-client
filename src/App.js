8c33deadbebcaa713dacb268e4618a88a9d20c75
swadhin503 <tawhiddwinswadhin@gmail.com>
2022-05-05 02:12:26 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Header from './components/Header/Header';
 function App() {
   return (
     <div>
-      <Header>as={Link}</Header>
+      <Header></Header>
       <h1>Home</h1>
     </div>
   );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <div>
             <h1>This is header</h1>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Container>
+                <Navbar.Brand href="#home">Laptop's Warehouse</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                    <Nav.Link as={Link} to="/home" href="#home">Home</Nav.Link>
+                    <Nav.Link as={Link} to="/items" href="#items">Items</Nav.Link>
+                    </Nav>
+                    <Nav>
+                    <Nav.Link as={Link} to="/aboutus" href="#about_us">About Us</Nav.Link>
+                    <Nav.Link as={Link} to="/login" href="#memes">Login</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </div>
     );
 };
