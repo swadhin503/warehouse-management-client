2c899dd2c0968b76543cce3f6c3b399c3ee4e998
swadhin503 <tawhiddwinswadhin@gmail.com>
2022-05-06 02:16:36 +0600

    banner added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,51 @@
 import React from 'react';
+import { Carousel } from 'react-bootstrap';
+import img1 from '../../images/banner-1.jpg'
+import img2 from '../../images/banner-2.jpg'
+import img3 from '../../images/banner-3.jpg'
 
 const Home = () => {
     return (
         <div>
-            <h2>This is home</h2>
+            <Carousel className="mt-1">
+                <Carousel.Item>
+                    <img
+                    className="d-block w-100"
+                    src={img1}
+                    alt="First slide"
+                    />
+                    <Carousel.Caption>
+                    <h3>The Best Products</h3>
+                    <p>Explore uur website and order now to get the best products.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                    className="d-block w-100"
+                    src={img2}
+                    alt="Second slide"
+                    />
+
+                    <Carousel.Caption>
+                    <h3>The Best Products</h3>
+                    <p>Explore uur website and order now to get the best products.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                    className="d-block w-100"
+                    src={img3}
+                    alt="Third slide"
+                    />
+
+                    <Carousel.Caption>
+                    <h3>The Best Products</h3>
+                    <p>Explore uur website and order now to get the best products.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
+
+
         </div>
     );
 };
